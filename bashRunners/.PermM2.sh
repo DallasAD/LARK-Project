fe7bedd77f6b0f2a1d70d3c96da628d79344a75f
DallasAD <94092664+DallasAD@users.noreply.github.com>
2024-01-15 15:22:16 -0500

    Delete bashRunners directory

--- bashRunners/.PermM2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#reset permissions when game is transported to a new user
-
-chmod 000 .Machine2/Downloads/Apps/Zmail.exe
-
--- bashRunners/.ResetMachine2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-chmod 777 Machine2/Downloads/Apps/Zmail.exe
